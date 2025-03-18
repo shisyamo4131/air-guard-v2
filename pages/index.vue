df1bde6a5132b79f75c08ab4381d04ae70744d77
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-03-18 11:28:09 +0900

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,78 +1,61 @@
 <script setup>
-import { FireModel } from "air-firebase-v2";
-
-const addTestData = async () => {
-  try {
-    const model = new FireModel();
-    await model.create();
-    alert("データ追加成功");
-    const location = await fetchCoordinates("東京都");
-    console.log(location);
-  } catch (err) {
-    console.error("Firestore 書き込みエラー:", err);
-  }
-};
-
+import User from "../models/User";
 const dialog = ref(false);
-const Customer = ref({
-  name: "唯心",
-});
+const Model = new User();
 </script>
 
 <template>
-  <div>
-    <RenderlessItemManager v-model="Customer" :is-editing="dialog">
-      <template
-        #default="{
-          isEditing,
-          item,
-          quitEditing,
-          submit,
-          updateProperties,
-          toUpdate,
-        }"
-      >
-        <v-container>
-          <v-card>
-            <v-card-title>Customer</v-card-title>
-            <v-card-text>
-              {{ Customer }}
-            </v-card-text>
-            <v-card-actions>
-              <v-dialog
-                :model-value="isEditing"
-                @update:modelValue="quitEditing"
-              >
-                <template #activator="{ props: attrs }">
-                  <v-btn
-                    v-bind="attrs"
-                    color="primary"
-                    variant="elevated"
-                    @click="toUpdate"
-                    >open</v-btn
-                  >
-                </template>
-                <v-card>
-                  <v-card-title>edit</v-card-title>
-                  <v-card-text>
-                    <v-text-field
-                      :value="item.name"
-                      variant="outlined"
-                      @update:model-value="updateProperties({ name: $event })"
-                    />
-                  </v-card-text>
-                  <v-card-actions>
-                    <v-btn @click="submit">submit</v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
-            </v-card-actions>
-          </v-card>
-        </v-container>
-        <div>{{ Customer }}</div>
-        <div>{{ item }}</div>
-      </template>
-    </RenderlessItemManager>
-    <button @click="Customer.name = 'daizo'">aaaa</button>
-  </div>
+  <RenderlessItemManager v-model="Model" :is-editing="dialog">
+    <template
+      #default="{
+        isEditing,
+        item,
+        quitEditing,
+        submit,
+        updateProperties,
+        toUpdate,
+      }"
+    >
+      <v-container>
+        <v-card>
+          <v-card-title>User</v-card-title>
+          <v-card-text>
+            {{ Model }}
+          </v-card-text>
+          <v-card-actions>
+            <v-dialog :model-value="isEditing" @update:modelValue="quitEditing">
+              <template #activator="{ props: attrs }">
+                <v-btn
+                  v-bind="attrs"
+                  color="primary"
+                  variant="elevated"
+                  @click="toUpdate"
+                  >open</v-btn
+                >
+              </template>
+              <v-card>
+                <v-card-title>edit</v-card-title>
+                <v-card-text>
+                  <v-text-field
+                    :model-value="item.email"
+                    variant="outlined"
+                    @update:model-value="updateProperties({ email: $event })"
+                  />
+                  <v-checkbox
+                    :model-value="item.isAdmin"
+                    @update:model-value="updateProperties({ isAdmin: $event })"
+                  />
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn @click="submit">submit</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-card-actions>
+        </v-card>
+      </v-container>
+      <div>{{ Model }}</div>
+      <div>{{ item }}</div>
+    </template>
+  </RenderlessItemManager>
 </template>
