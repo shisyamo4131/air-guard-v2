a376ea6efdcf1e1e53811d8070e49f176dadf70e
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-04-17 14:29:37 +0900

    change: v-app-bar を追加

--- layouts/guest.vue
+++ layouts/guest.vue
@@ -8,6 +8,11 @@ const { queue } = useLoadingsStore();
     <!-- グローバルローディング状態を表示するダイアログ -->
     <air-loading-dialog :model-value="queue" />
 
+    <!-- アプリケーション上部のツールバー -->
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-title>AirGuard</v-app-bar-title>
+    </v-app-bar>
+
     <!-- メインコンテンツ表示領域 -->
     <v-main>
       <NuxtPage />
