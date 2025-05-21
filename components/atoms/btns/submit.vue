877975a1d3792322a6be0bc971d139402bc06943
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-05-21 12:01:52 +0900

    commit

--- components/atoms/btns/submit.vue
+++ components/atoms/btns/submit.vue
@@ -1,22 +1,13 @@
 <script setup>
 /**
- * 汎用的な送信・確定アクションを表すボタンコンポーネントです。
- * デフォルトでチェックアイコンが表示され、「確定」というテキストが表示されます。
- * テキストはスロットまたは `label` プロパティで、アイコンは `prependIcon` プロパティでカスタマイズ可能です。
+ * @file AtomsBtnsSubmit.vue
+ * @description
+ * 固定デザインの「確定」ボタンコンポーネントです。
+ * 「mdi-check」アイコンと「確定」テキストが表示されます。
  */
 defineOptions({ inheritAttrs: false, name: "AtomsBtnsSubmit" });
-const props = defineProps({
-  /** ボタンのデフォルトテキスト。スロットが指定されていない場合に使用されます。 */
-  label: { type: String, default: "確定" },
-  /** ボタンの先頭に表示するアイコン。`null` を指定するとアイコンなしになります。 */
-  prependIcon: { type: String, default: "mdi-check" },
-});
 </script>
 
 <template>
-  <v-btn v-bind="$attrs" :prepend-icon="props.prependIcon">
-    <slot name="default">
-      {{ label }}
-    </slot>
-  </v-btn>
+  <v-btn v-bind="$attrs" prepend-icon="mdi-check"> 確定 </v-btn>
 </template>
---
+++ components/molecules/cards/editor.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineOptions({ inheritAttrs: false });
+const props = defineProps({
+  label: { type: String, default: undefined },
+  disableSubmit: { type: Boolean, default: false },
+});
+const emit = defineEmits(["click:close", "click:submit"]);
+</script>
+
+<template>
+  <v-card v-bind="$attrs">
+    <v-toolbar>
+      <v-toolbar-title>{{ label }}</v-toolbar-title>
+      <v-spacer />
+      <AtomsBtnsCloseIcon @click="emit('click:close')" />
+    </v-toolbar>
+    <v-card-text>
+      <slot name="default" />
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer />
+      <AtomsBtnsSubmit
+        color="primary"
+        :disabled="disableSubmit"
+        variant="elevated"
+        @click="emit('click:submit')"
+      />
+    </v-card-actions>
+  </v-card>
+</template>
