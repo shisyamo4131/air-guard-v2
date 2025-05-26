2a9ea765f31f051f47b578ecd17f108450d76799
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-05-26 18:46:11 +0900

    refact

--- components/atoms/inputs/TextFieldZipcode.vue
+++ components/atoms/inputs/TextFieldZipcode.vue
@@ -1,9 +1,15 @@
 <script setup>
+/**
+ * @file AtomsInputsTextFieldZipcode.vue
+ * @description 郵便番号入力用コンポーネント
+ */
 defineOptions({ name: "AtomsInputsTextFieldZipcode" });
+
 const model = defineModel();
+
 const emit = defineEmits(["update:address"]);
+
 watch(model, async (newVal) => {
-  console.log("fetchedAddress");
   if (!newVal || newVal.length !== 7) return;
   const fetchedAddress = await fetchAddressFromPostalCode(newVal);
   if (fetchedAddress) {
