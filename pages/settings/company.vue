fcd072c322a07181664c7f25445fef699faffa30
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-05-26 15:03:37 +0900

    commit

--- pages/settings/company.vue
+++ pages/settings/company.vue
@@ -1,60 +1,8 @@
-<script setup>
-import { Company } from "air-guard-v2-schemas";
-const auth = useAuthStore();
-const { company } = storeToRefs(auth);
-
-// --- 表示フィールドの定義 ---
-const companyFields = Object.keys(Company.classProps);
-</script>
+<script setup></script>
 
 <template>
   <v-container>
-    <ItemManager :schema="company" v-slot="slotProps" label="会社情報">
-      <v-dialog v-bind="slotProps.dialogProps">
-        <MoleculesCardsEditor v-bind="slotProps.editorProps">
-          <air-item-input v-bind="slotProps" :schema="Company.schema">
-            <template #prefecture="{ field, modelValue, updateModelValue }">
-              <AtomsSelectPrefecture
-                :model-value="modelValue"
-                :label="field.label"
-                :required="field.required"
-                @update:model-value="updateModelValue"
-              />
-            </template>
-          </air-item-input>
-        </MoleculesCardsEditor>
-      </v-dialog>
-      <v-card class="mx-auto" elevation="2">
-        <v-card-title> 会社情報 </v-card-title>
-        <v-container>
-          <v-list lines="two" density="compact">
-            <template v-for="field in companyFields" :key="field">
-              <v-list-item>
-                <v-list-item-subtitle>
-                  {{ Company.classProps[field]?.label || field }}
-                </v-list-item-subtitle>
-                <v-list-item-title>
-                  {{ company[field] || "-" }}
-                </v-list-item-title>
-              </v-list-item>
-              <v-divider
-                v-if="field !== companyFields[companyFields.length - 1]"
-              ></v-divider>
-            </template>
-          </v-list>
-        </v-container>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="primary"
-            variant="elevated"
-            @click="slotProps.toUpdate()"
-          >
-            編集する
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </ItemManager>
+    <OrganismsCompanyManager />
   </v-container>
 </template>
 
