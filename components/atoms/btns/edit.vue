a8d8d2c0ce921a00313a799d921e140e18a94587
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-05-27 11:55:23 +0900

    add: 新規実装

---
+++ components/atoms/btns/edit.vue
@@ -0,0 +1,12 @@
+<script setup>
+/**
+ * @file AtomsBtnsEdit.vue
+ * @description 固定デザインの編集ボタンコンポーネントです。
+ *  `mdi-pencil` アイコンと `編集` テキストが表示されます。
+ */
+defineOptions({ inheritAttrs: false, name: "AtomsBtnsEdit" });
+</script>
+
+<template>
+  <v-btn v-bind="$attrs" prepend-icon="mdi-pencil"> 編集 </v-btn>
+</template>
