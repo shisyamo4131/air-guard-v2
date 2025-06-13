d9ef2070ff2f60289dac48602c8b0b37f60d77ba
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-06-13 16:21:32 +0900

    commit

--- pages/collection-routes/[id].vue
+++ pages/collection-routes/[id].vue
@@ -5,28 +5,12 @@
  * - ルートパラメータ [id] は CollectionRoutes コレクションのドキュメントID
  * - ドキュメント id をもとに CollectionRoute クラスからドキュメント情報を取得して表示
  */
-import { reactive, onMounted, computed, onUnmounted } from "vue";
+import { reactive, onMounted, onUnmounted } from "vue";
 import { useRoute } from "vue-router";
-import { useLogger } from "~/composables/useLogger";
 import { CollectionRoute } from "~/schemas/CollectionRoute";
-
-const logger = useLogger();
 
 const route = useRoute();
 const model = reactive(new CollectionRoute());
-
-const items = computed(() => {
-  return [
-    {
-      title: "ルートコード",
-      props: { subtitle: model.code, prependIcon: "mdi-identifier" },
-    },
-    {
-      title: "ルート名",
-      props: { subtitle: model.name, prependIcon: "mdi-map-marker" },
-    },
-  ];
-});
 
 onMounted(async () => {
   const docId = route.params.id;
@@ -43,10 +27,47 @@ onUnmounted(() => {
     <v-row>
       <v-col>
         <v-card>
-          <v-card-item>
-            <v-card-title>{{ model.name }}</v-card-title>
-          </v-card-item>
-          <v-list :items="items"> </v-list>
+          <v-toolbar>
+            <v-toolbar-title>{{ model.name }}</v-toolbar-title>
+            <v-spacer />
+            <ItemManager
+              :model="model"
+              v-slot="slotProps"
+              label="回収ルート情報"
+            >
+              <v-dialog v-bind="slotProps.dialogProps">
+                <template #activator>
+                  <v-btn icon="mdi-pencil" @click="slotProps.toUpdate()" />
+                </template>
+                <MoleculesCardsEditor v-bind="slotProps.editorProps">
+                  <air-item-input
+                    v-bind="slotProps"
+                    :schema="CollectionRoute.schema"
+                  />
+                </MoleculesCardsEditor>
+              </v-dialog>
+            </ItemManager>
+          </v-toolbar>
+          <v-list>
+            <v-list-item>
+              <template #prepend>
+                <v-icon>mdi-identifier</v-icon>
+              </template>
+              <v-list-item-subtitle> CODE </v-list-item-subtitle>
+              <v-list-item-title>
+                {{ model.code }}
+              </v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <template #prepend>
+                <v-icon>mdi-tag-outline</v-icon>
+              </template>
+              <v-list-item-subtitle> ルート名 </v-list-item-subtitle>
+              <v-list-item-title>
+                {{ model.name }}
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
         </v-card>
       </v-col>
       <v-col cols="12">
