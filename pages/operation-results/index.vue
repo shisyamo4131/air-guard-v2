85c8473e33b80d9ca7877a6e799cd53b954014b3
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-06-23 11:26:27 +0900

    upgrade: 日付、現場名の表示機能を改善

--- pages/operation-results/index.vue
+++ pages/operation-results/index.vue
@@ -5,13 +5,22 @@
  */
 import { OperationResult } from "@/schemas";
 import { reactive, onMounted, onUnmounted } from "vue";
+import { useFetchSite } from "@/composables/useFetchSite";
+import dayjs from "dayjs";
+
+const { fetchSite, cachedSites, isLoading: isSitesLoading } = useFetchSite();
 
 const operationResult = reactive(new OperationResult());
 const docs = computed(() => operationResult.docs);
+watch(docs, (newVal) => fetchSite(newVal), { deep: true });
 
 const headers = [
-  { title: "code", value: "code" },
-  { title: "現場名", value: "name" },
+  {
+    title: "日付",
+    key: "date",
+    value: (item) => dayjs(item.date).format("MM月DD日(ddd)"),
+  },
+  { title: "現場名", key: "siteId", value: "siteId" },
   { title: "操作", value: "actions", align: "end", sortable: false },
 ];
 
@@ -47,12 +56,15 @@ onUnmounted(() => {
           />
         </MoleculesCardsEditor>
       </v-dialog>
-      <v-data-table :items="docs" :headers="headers">
+      <v-data-table :items="docs" :headers="headers" :loading="isSitesLoading">
         <template #top>
           <v-toolbar density="compact" flat>
             <v-toolbar-title>稼働実績一覧</v-toolbar-title>
             <v-btn icon="mdi-plus" @click="slotProps.toCreate()"></v-btn>
           </v-toolbar>
+        </template>
+        <template v-slot:item.siteId="{ item }">
+          {{ cachedSites[item.siteId]?.name || "" }}
         </template>
         <template v-slot:item.actions="{ item }">
           <div class="d-flex ga-2 justify-end">
