50e616859f27cace6d98f5888bee3bb10ab12787
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-06-23 11:26:44 +0900

    upgrade:

--- pages/operation-results/[id].vue
+++ pages/operation-results/[id].vue
@@ -1,89 +1,76 @@
 <script setup>
 /**
- * @file ./pages/sites/[id].vue
- * @description 現場情報詳細ページ
- * - ルートパラメータ [id] は Sites コレクションのドキュメント id
- * - ドキュメント id をもとに Site クラスからドキュメント情報を取得して表示
+ * @file ./pages/operation-results/[id].vue
+ * @description 稼働実績詳細ページ
+ * - ルートパラメータ [id] は OperationResults コレクションのドキュメント id
+ * - ドキュメント id をもとに OperationResult クラスからドキュメント情報を取得して表示
  */
 import dayjs from "dayjs";
-import { reactive, onMounted, computed, onUnmounted, ref, watch } from "vue";
+import { reactive, onMounted, computed, onUnmounted } from "vue";
 import { useRoute } from "vue-router";
-import { Site, SiteOperationSchedule } from "~/schemas";
+import { OperationResult } from "~/schemas";
 
 const route = useRoute();
-const siteId = route.params.id;
-const model = reactive(new Site());
-const scheduleInstance = reactive(new SiteOperationSchedule({ siteId }));
-const schedules = ref([]);
-
-const currentDate = ref([new Date()]);
-const period = computed(() => {
-  const start = dayjs(currentDate.value[0]).startOf("month").toDate();
-  const end = dayjs(currentDate.value[0]).endOf("month").toDate();
-  return { start, end };
-});
+const operationResultId = route.params.id;
+const model = reactive(new OperationResult());
 
 const items = computed(() => {
   return [
     {
-      title: "CODE",
-      props: { subtitle: model.code, prependIcon: "mdi-magnify" },
-    },
-    {
-      title: "住所",
-      props: { subtitle: model.fullAddress, prependIcon: "mdi-map-marker" },
-    },
-    {
-      title: "取引先",
+      title: "日付",
       props: {
-        subtitle: model.customer?.name || "loading",
-        prependIcon: "mdi-domain",
+        subtitle: dayjs(model.date).format("YYYY-MM-DD"),
+        prependIcon: "mdi-calendar",
       },
+    },
+    {
+      title: "曜日区分",
+      props: {
+        subtitle: model.dayType,
+        prependIcon: "mdi-calendar",
+      },
+    },
+    {
+      title: "勤務区分",
+      props: {
+        subtitle: model.shiftType,
+        prependIcon: "mdi-calendar",
+      },
+    },
+    {
+      title: "現場",
+      props: { subtitle: model.siteId, prependIcon: "mdi-map-marker" },
     },
   ];
 });
 
 onMounted(async () => {
-  await model.subscribe({ docId: siteId });
+  await model.subscribe({ docId: operationResultId });
 });
-
-watch(
-  period,
-  async (newVal) => {
-    const docId = route.params.id;
-    if (docId && newVal && newVal.start && newVal.end) {
-      schedules.value = await scheduleInstance.subscribeDocs({
-        constraints: [
-          ["where", "siteId", "==", docId],
-          ["where", "startDate", ">=", newVal.start],
-          ["where", "startDate", "<=", newVal.end],
-        ],
-      });
-    }
-  },
-  { immediate: true, deep: true }
-);
 
 onUnmounted(() => {
   model.unsubscribe();
-  scheduleInstance.unsubscribe();
 });
 </script>
 <template>
   <v-container>
     <v-row>
-      <v-col>
+      <v-col cols="12">
         <v-card>
           <v-toolbar density="comfortable">
-            <v-toolbar-title>{{ model.name }}</v-toolbar-title>
+            <v-toolbar-title>稼働実績</v-toolbar-title>
             <v-spacer />
-            <ItemManager :model="model" v-slot="slotProps" label="現場情報">
+            <ItemManager :model="model" v-slot="slotProps" label="稼働実績">
               <v-dialog v-bind="slotProps.dialogProps">
                 <template #activator>
                   <v-btn icon="mdi-pencil" @click="slotProps.toUpdate()" />
                 </template>
                 <MoleculesCardsEditor v-bind="slotProps.editorProps">
-                  <air-item-input v-bind="slotProps" :schema="Site.schema" />
+                  <air-item-input
+                    v-bind="slotProps"
+                    :schema="OperationResult.schema"
+                    :excluded-keys="['workers']"
+                  />
                 </MoleculesCardsEditor>
               </v-dialog>
             </ItemManager>
@@ -92,36 +79,42 @@ onUnmounted(() => {
         </v-card>
       </v-col>
       <v-col cols="12">
-        <ItemManager
-          :model="scheduleInstance"
-          v-slot="slotProps"
-          label="稼働予定"
-        >
-          <v-dialog v-bind="slotProps.dialogProps">
-            <MoleculesCardsEditor v-bind="slotProps.editorProps">
-              <air-item-input
-                v-bind="slotProps"
-                :schema="SiteOperationSchedule.schema"
-              >
-              </air-item-input>
-            </MoleculesCardsEditor>
-          </v-dialog>
-          <v-card>
-            <v-toolbar density="comfortable">
-              <v-toolbar-title>稼働予定</v-toolbar-title>
-              <v-spacer />
-              <v-btn icon="mdi-plus" @click="slotProps.toCreate()" />
-            </v-toolbar>
-            <v-container class="pt-0">
-              <air-calendar
-                v-model="currentDate"
-                :events="schedules.map((schedule) => schedule.toEvent())"
-                hide-week-number
-                @click:event="slotProps.toUpdate($event.item)"
-              />
-            </v-container>
-          </v-card>
-        </ItemManager>
+        <v-card>
+          <v-toolbar density="comfortable">
+            <v-toolbar-title>稼働実績明細</v-toolbar-title>
+            <v-spacer />
+            <ItemManager :model="model" v-slot="slotProps" label="稼働実績明細">
+              <v-dialog v-bind="slotProps.dialogProps">
+                <template #activator>
+                  <v-btn icon="mdi-pencil" @click="slotProps.toUpdate()" />
+                </template>
+                <MoleculesCardsEditor
+                  v-bind="slotProps.editorProps"
+                  hide-delete-btn
+                >
+                  <air-item-input
+                    v-bind="slotProps"
+                    :schema="OperationResult.schema"
+                    :included-keys="['workers']"
+                  />
+                </MoleculesCardsEditor>
+              </v-dialog>
+            </ItemManager>
+          </v-toolbar>
+          <v-data-table
+            :headers="[
+              { title: '氏名' },
+              { title: '開始時刻' },
+              { title: '終了時刻' },
+              { title: '休憩時間' },
+              { title: '残業時間' },
+              { title: '資格者' },
+              { title: 'OJT' },
+            ]"
+            hide-default-footer
+            items-per-page="-1"
+          />
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
