83c3839444f9c560e864fb717428edbb4e9946e9
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-06-24 14:45:20 +0900

    add: 新規実装

---
+++ components/molecules/AutocompleteEmployee.vue
@@ -0,0 +1,30 @@
+<script setup>
+/**
+ * @file @/components/molecules/AutocompleteEmployee.vue
+ * @description 従業員選択用 Autocomplete コンポーネント
+ */
+import { Employee } from "@/schemas";
+
+const props = defineProps({
+  api: {
+    type: Function,
+    default: async (search) => {
+      return await new Employee().fetchDocs({
+        constraints: search,
+      });
+    },
+  },
+  label: { type: String, default: "従業員" },
+  itemTitle: { type: String, default: "fullName" },
+  itemValue: { type: String, default: "docId" },
+});
+</script>
+
+<template>
+  <air-autocomplete-api
+    v-bind="{ ...props, ...$attrs }"
+    autocomplete="off"
+    no-cache
+  >
+  </air-autocomplete-api>
+</template>
