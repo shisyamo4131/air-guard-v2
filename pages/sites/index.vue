7a99602263d93cb80805e3143b79c5df189c0232
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-07-04 20:11:35 +0900

    change:
    - SitesManager コンポーネントを削除し、sites/index.vue を再実装。

--- pages/sites/index.vue
+++ pages/sites/index.vue
@@ -3,11 +3,30 @@
  * @file pages/settings/sites.vue
  * @description 現場管理画面
  */
+import { Site } from "@/schemas";
+import { reactive } from "vue";
+
+const model = reactive(new Site());
+const docs = ref([]);
+
+onMounted(() => {
+  docs.value = model.subscribeDocs();
+});
+
+onUnmounted(() => {
+  model.unsubscribe();
+});
 </script>
 
 <template>
   <v-container>
-    <OrganismsSitesManager
+    <array-manager
+      v-model="docs"
+      :schema="Site"
+      v-slot="slotProps"
+      :input-props="{
+        excludedKeys: ['agreements'],
+      }"
       :before-edit="
         (editMode, item) => {
           if (editMode === 'CREATE') return true;
@@ -15,6 +34,15 @@
           return false;
         }
       "
-    />
+      :handle-create="(item) => item.create()"
+      @create="($event) => $router.push(`sites/${$event.docId}`)"
+    >
+      <air-data-table v-bind="slotProps.tableProps" />
+      <v-dialog v-bind="slotProps.dialogProps">
+        <MoleculesCardsEditor v-bind="slotProps.editorProps">
+          <air-item-input v-bind="slotProps.inputProps" />
+        </MoleculesCardsEditor>
+      </v-dialog>
+    </array-manager>
   </v-container>
 </template>
