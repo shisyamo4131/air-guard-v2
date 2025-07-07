779203f984f9c7f9d2f866485c3a55a8f430afdf
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-07-07 12:12:34 +0900

    change: 会社管理機能の再実装（CompanyManager を廃止）

--- pages/settings/company.vue
+++ pages/settings/company.vue
@@ -1,8 +1,52 @@
-<script setup></script>
+<script setup>
+/**
+ * @file organisms/CompanyManager.vue
+ * @description 会社情報管理
+ */
+import { Company } from "@/schemas";
+
+/** ストア連携 */
+const auth = useAuthStore();
+
+const { company } = storeToRefs(auth);
+
+// --- 表示フィールドの定義 ---
+const companyFields = Object.keys(Company.classProps);
+</script>
 
 <template>
   <v-container>
-    <OrganismsCompanyManager />
+    <ItemManager :model="company" v-slot="slotProps" label="会社情報">
+      <v-dialog v-bind="slotProps.dialogProps">
+        <MoleculesCardsEditor v-bind="slotProps.editorProps" hide-delete-btn>
+          <air-item-input v-bind="slotProps" :schema="Company.schema" />
+        </MoleculesCardsEditor>
+      </v-dialog>
+      <v-card class="mx-auto" elevation="2">
+        <v-card-title> 会社情報 </v-card-title>
+        <v-container>
+          <v-list lines="two" density="compact">
+            <template v-for="field in companyFields" :key="field">
+              <v-list-item>
+                <v-list-item-subtitle>
+                  {{ Company.classProps[field]?.label || field }}
+                </v-list-item-subtitle>
+                <v-list-item-title>
+                  {{ company[field] || "-" }}
+                </v-list-item-title>
+              </v-list-item>
+              <v-divider
+                v-if="field !== companyFields[companyFields.length - 1]"
+              ></v-divider>
+            </template>
+          </v-list>
+        </v-container>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <AtomsBtnsEdit color="primary" @click="slotProps.toUpdate()" />
+        </v-card-actions>
+      </v-card>
+    </ItemManager>
   </v-container>
 </template>
 
