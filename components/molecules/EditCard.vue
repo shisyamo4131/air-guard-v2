cb0fa9d8c455e440cb31a3b9664e4af180b520b4
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-07-07 15:34:50 +0900

    add: 新規実装

---
+++ components/molecules/EditCard.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useLogger } from "~/composables/useLogger";
+
+// --- ストア / コンポーザブル
+const logger = useLogger();
+</script>
+
+<template>
+  <air-edit-card :logger="logger" v-slot="slotProps">
+    <slot name="default" v-bind="slotProps" />
+  </air-edit-card>
+</template>
