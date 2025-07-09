3861d734d112f83d9a3d9d5b11ff799b91904798
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-07-09 13:20:17 +0900

    add:
    - 現場稼働予定管理コンポーネント `SiteOperationSchedulesManager.vue` を新規実装。
    - 現場稼働予定管理機能をコンポーネントを使用するように変更。

---
+++ components/organisms/SiteOperationSchedulesManager.vue
@@ -0,0 +1,112 @@
+<script setup>
+/**
+ * @file @/components/organisms/SiteOperationScheduleManager.vue
+ * @description A component that manages the site operation schedule.
+ */
+import dayjs from "dayjs";
+import { SiteOperationSchedule } from "@/schemas";
+
+/** define-options */
+defineOptions({ name: "SiteOperationScheduleManager" });
+
+/** define-props */
+const props = defineProps({
+  siteId: { type: String, required: true },
+});
+
+/** SiteOperationSchedule instance */
+const instance = reactive(new SiteOperationSchedule());
+
+/** A date for the current month */
+const currentDate = ref([new Date()]);
+
+/** Period of the current month calculated from currentDate */
+const period = computed(() => {
+  const start = dayjs(currentDate.value[0]).startOf("month").toDate();
+  const end = dayjs(currentDate.value[0]).endOf("month").toDate();
+  return { start, end };
+});
+
+/**
+ * Start subscribing to the SiteOperationSchedule documents
+ * when the period changes or the component is mounted.
+ */
+watch(
+  period,
+  async (newVal) => {
+    const siteId = props.siteId;
+    const [startAt, endAt] = [newVal.start, newVal.end];
+    if (!siteId || !startAt || !endAt) return;
+    subscribe(siteId, startAt, endAt);
+  },
+  { immediate: true, deep: true }
+);
+
+/**
+ * Start subscribing to the SiteOperationSchedule documents
+ * when the period changes or the component is mounted.
+ * @param siteId
+ * @param startAt
+ * @param endAt
+ */
+function subscribe(siteId, startAt, endAt) {
+  instance.subscribeDocs({
+    constraints: [
+      ["where", "siteId", "==", siteId],
+      ["where", "startAt", ">=", startAt],
+      ["where", "startAt", "<=", endAt],
+    ],
+  });
+}
+
+onUnmounted(() => {
+  instance.unsubscribe();
+});
+</script>
+
+<template>
+  <ArrayManager
+    v-slot="slotProps"
+    :model-value="instance.docs"
+    :schema="SiteOperationSchedule"
+    :before-edit="(editMode, item) => (item.siteId = siteId)"
+    :handle-create="async (item) => await item.create()"
+    :handle-update="async (item) => await item.update()"
+    :handle-delete="async (item) => await item.delete()"
+  >
+    <v-dialog v-bind="slotProps.dialogProps">
+      <MoleculesEditCard v-bind="slotProps.editorProps">
+        <air-item-input v-bind="slotProps.inputProps">
+          <template #startAt="{ attrs }">
+            <air-date-time-picker
+              v-bind="attrs"
+              @update:modelValue="
+                ($event) => {
+                  // $event (= startAt) の 9時間後 を取得して endAt を更新
+                  const endAt = new Date($event);
+                  endAt.setHours(endAt.getHours() + 9);
+                  slotProps.updateProperties({ endAt });
+                }
+              "
+            />
+          </template>
+        </air-item-input>
+      </MoleculesEditCard>
+    </v-dialog>
+    <v-card>
+      <v-toolbar density="comfortable">
+        <v-toolbar-title>稼働予定</v-toolbar-title>
+        <v-spacer />
+        <v-btn icon="mdi-plus" @click="slotProps.toCreate()" />
+      </v-toolbar>
+      <v-container class="pt-0">
+        <air-calendar
+          v-model="currentDate"
+          :events="slotProps.items.map((schedule) => schedule.toEvent())"
+          hide-week-number
+          @click:event="slotProps.toUpdate($event.item)"
+        />
+      </v-container>
+    </v-card>
+  </ArrayManager>
+</template>
--- pages/sites/[id].vue
+++ pages/sites/[id].vue
@@ -5,17 +5,13 @@
  * - ルートパラメータ [id] は Sites コレクションのドキュメント id
  * - ドキュメント id をもとに Site クラスからドキュメント情報を取得して表示
  */
-import dayjs from "dayjs";
 import { reactive, onMounted, computed, onUnmounted, ref, watch } from "vue";
 import { useRoute } from "vue-router";
-import { Site, Agreement, SiteOperationSchedule } from "~/schemas";
+import { Site, Agreement } from "~/schemas";
 
 const route = useRoute();
 const siteId = route.params.id;
 const model = reactive(new Site());
-const scheduleInstance = reactive(new SiteOperationSchedule({ siteId }));
-
-const currentDate = ref([new Date()]);
 
 const items = computed(() => {
   return [
@@ -37,35 +33,12 @@ const items = computed(() => {
   ];
 });
 
-const period = computed(() => {
-  const start = dayjs(currentDate.value[0]).startOf("month").toDate();
-  const end = dayjs(currentDate.value[0]).endOf("month").toDate();
-  return { start, end };
-});
-watch(
-  period,
-  async (newVal) => {
-    const docId = route.params.id;
-    if (docId && newVal && newVal.start && newVal.end) {
-      scheduleInstance.subscribeDocs({
-        constraints: [
-          ["where", "siteId", "==", docId],
-          ["where", "startAt", ">=", newVal.start],
-          ["where", "startAt", "<=", newVal.end],
-        ],
-      });
-    }
-  },
-  { immediate: true, deep: true }
-);
-
 onMounted(async () => {
   await model.subscribe({ docId: siteId });
 });
 
 onUnmounted(() => {
   model.unsubscribe();
-  scheduleInstance.unsubscribe();
 });
 </script>
 <template>
@@ -88,36 +61,7 @@ onUnmounted(() => {
         </v-card>
       </v-col>
       <v-col cols="12">
-        <ArrayManager
-          v-slot="slotProps"
-          :model-value="scheduleInstance.docs"
-          :schema="SiteOperationSchedule"
-          :before-edit="(editMode, item) => (item.siteId = siteId)"
-          :handle-create="async (item) => await item.create()"
-          :handle-update="async (item) => await item.update()"
-          :handle-delete="async (item) => await item.delete()"
-        >
-          <v-dialog v-bind="slotProps.dialogProps">
-            <MoleculesEditCard v-bind="slotProps.editorProps">
-              <air-item-input v-bind="slotProps.inputProps" />
-            </MoleculesEditCard>
-          </v-dialog>
-          <v-card>
-            <v-toolbar density="comfortable">
-              <v-toolbar-title>稼働予定</v-toolbar-title>
-              <v-spacer />
-              <v-btn icon="mdi-plus" @click="slotProps.toCreate()" />
-            </v-toolbar>
-            <v-container class="pt-0">
-              <air-calendar
-                v-model="currentDate"
-                :events="slotProps.items.map((schedule) => schedule.toEvent())"
-                hide-week-number
-                @click:event="slotProps.toUpdate($event.item)"
-              />
-            </v-container>
-          </v-card>
-        </ArrayManager>
+        <OrganismsSiteOperationSchedulesManager :site-id="siteId" />
       </v-col>
       <v-col>
         <v-card>
