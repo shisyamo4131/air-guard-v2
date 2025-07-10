83f3701ed8f7bebcb2414292f75ae1918ea87c59
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-07-10 14:34:57 +0900

    upgrade:
    - 明細の削除機能を実装。
    - その他軽微なリファクタ。

--- components/operation-results/DataTable.vue
+++ components/operation-results/DataTable.vue
@@ -1,9 +1,9 @@
 <script setup>
 /**
  * @file @/components/operation-results/DataTable.vue
- * @description 稼働実績明細用のデータテーブルコンポーネント
- * - `props.isEditing` が true を受け取ると編集用の表示に切り替わります。
- * - 氏名の表示には `props.employees` を与えられる必要があります。
+ * @description Data table for operation result details(employees).
+ * This component displays a table of employees with their operation results,
+ * allowing for editing of start and end times, break times, overtime, and qualifications.
  */
 import dayjs from "dayjs";
 import { OperationResultEmployee } from "@/schemas";
@@ -18,25 +18,15 @@ const props = defineProps({
 /** define emits */
 const emit = defineEmits(["update:items"]);
 
-/** An array to manage internal items */
+/**
+ * An array to manage internal items
+ * - Initialized with `props.items` mapped to `OperationResultEmployee` instances by watcher.
+ */
 const internalItems = ref([]);
-watch(
-  () => props.items,
-  (newVal) => {
-    internalItems.value = newVal.map((item) => {
-      return new OperationResultEmployee(item);
-    });
-  },
-  { immediate: true, deep: true }
-);
 
-/** テーブルのカラム設定 */
-const headers = [
-  {
-    title: "氏名",
-    key: "employeeId",
-    sortable: false,
-  },
+/** define table's header */
+const headers = ref([
+  { title: "氏名", key: "employeeId", sortable: false },
   { title: "開始時刻", key: "startAt", align: "center", sortable: false },
   { title: "終了時刻", key: "endAt", align: "center", sortable: false },
   {
@@ -55,10 +45,39 @@ const headers = [
   },
   { title: "資格者", key: "isQualificated", align: "center", sortable: false },
   { title: "OJT", key: "isOjt", align: "center", sortable: false },
-];
+]);
 
-/** AirAutocompleteApi によって選択された従業員ID */
+/** employee id selected at AutoCompleteApi */
 const selectedEmployeeId = ref(null);
+
+/*****************************************************************************
+ * COMPUTED PROPERTIES
+ *****************************************************************************/
+const computedHeaders = computed(() => {
+  const defaultHeaders = headers.value.map((header) => header);
+  if (props.isEditing) {
+    defaultHeaders.push({
+      title: "削除",
+      key: "deletion",
+      align: "end",
+      sortable: false,
+    });
+  }
+  return defaultHeaders;
+});
+
+/*****************************************************************************
+ * WATCHERS
+ *****************************************************************************/
+watch(
+  () => props.items,
+  (items) => {
+    internalItems.value = items.map(
+      (item) => new OperationResultEmployee(item)
+    );
+  },
+  { immediate: true, deep: true }
+);
 
 /** props.isEditing を監視 -> false に更新された selectedEmployeeId を初期化 */
 watch(
@@ -68,8 +87,17 @@ watch(
   }
 );
 
-/** 従業員追加ボタンがクリックされた時の処理 */
-function handleEmployeeIdAdded() {
+/*****************************************************************************
+ * FUNCTIONS
+ *****************************************************************************/
+/**
+ * Processing when an employee ID is selected.
+ * This function creates a new `OperationResultEmployee` instance
+ * and adds it to the `internalItems` array.
+ * It also emits an update to the parent component
+ * to reflect the changes in the items.
+ */
+function handleAddDetail() {
   if (!selectedEmployeeId.value) return;
   const newEmployee = new OperationResultEmployee({
     employeeId: selectedEmployeeId.value,
@@ -82,7 +110,24 @@ function handleEmployeeIdAdded() {
 }
 
 /**
- * DateオブジェクトをHH:mm形式の文字列にフォーマットします
+ * Remove a detail item from the internal items array.
+ * This function finds the index of the item to be deleted,
+ * removes it from the `internalItems` array,
+ * and emits an update to the parent component
+ * to reflect the changes in the items.
+ * @param item
+ */
+function handleRemoveDetail(item) {
+  const index = internalItems.value.indexOf(item);
+  if (index !== -1) {
+    internalItems.value.splice(index, 1);
+    emit("update:items", internalItems.value);
+  }
+}
+
+/**
+ * Format a date to a string in "YYYY-MM-DD HH:mm" format.
+ * This function uses the `dayjs` library to format the date.
  * @param {Date | string} date
  * @returns {string}
  */
@@ -93,7 +138,7 @@ function formatTime(date) {
 
 <template>
   <v-data-table
-    :headers="headers"
+    :headers="computedHeaders"
     hide-default-footer
     :items="internalItems"
     items-per-page="-1"
@@ -107,7 +152,7 @@ function formatTime(date) {
               v-model="selectedEmployeeId"
               append-icon="mdi-plus"
               icon-color="primary"
-              @click:append="handleEmployeeIdAdded"
+              @click:append="handleAddDetail"
             />
           </v-container>
         </div>
@@ -167,6 +212,11 @@ function formatTime(date) {
           emit('update:items', internalItems);
         "
       />
+    </template>
+
+    <!-- 削除ボタン -->
+    <template #item.deletion="{ item }">
+      <v-icon icon="mdi-trash-can" @click="handleRemoveDetail(item)" />
     </template>
   </v-data-table>
 </template>
