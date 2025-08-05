06a933f973fc84880af18a21ccc30b62db2cb5b1
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-08-05 15:55:41 +0900

    refactor: 現在の月の日付管理を簡素化し、setDateRangeの監視を削除

--- components/organisms/SiteOperationSchedulesManager.vue
+++ components/organisms/SiteOperationSchedulesManager.vue
@@ -15,31 +15,15 @@ const props = defineProps({
   siteId: { type: String, required: true },
 });
 
-/** A date for the current month */
-const currentDate = ref([new Date()]);
-
-/** Period of the current month calculated from currentDate */
-const period = computed(() => {
-  const from = dayjs(currentDate.value[0]).startOf("month").toDate();
-  const to = dayjs(currentDate.value[0]).endOf("month").toDate();
-  return { from, to };
-});
-
 const { company } = useAuthStore();
 
-const manager = useTemplateRef("manager");
-const { cachedData, arrayManagerAttrs, setDateRange } =
+const { cachedData, arrayManagerAttrs, calendarAttrs } =
   useSiteOperationScheduleManager({
-    manager,
+    manager: useTemplateRef("manager"),
     siteId: props.siteId,
-    from: period.value.from,
-    to: period.value.to,
+    from: dayjs().startOf("month").toDate(),
+    to: dayjs().endOf("month").toDate(),
   });
-
-/***************************************************************************
- * WATCHERS
- ***************************************************************************/
-watch(period, async (newVal) => setDateRange(newVal), { deep: true });
 </script>
 
 <template>
@@ -68,12 +52,7 @@ watch(period, async (newVal) => setDateRange(newVal), { deep: true });
           <v-btn icon="mdi-plus" @click="slotProps.toCreate()" />
         </v-toolbar>
         <v-container class="pt-0">
-          <air-calendar
-            v-model="currentDate"
-            :events="slotProps.items.map((schedule) => schedule.toEvent())"
-            hide-week-number
-            @click:event="slotProps.toUpdate($event.item)"
-          />
+          <air-calendar v-bind="calendarAttrs" />
         </v-container>
       </v-card>
     </slot>
