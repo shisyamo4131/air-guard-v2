fa5f48e203e3995d8c651ef850ed700ffd77070a
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-08-06 14:13:29 +0900

    refactor: タブの構成を整理し、従業員と外注先のデータを動的に取得するように変更

--- components/arrangements/WorkerSelector.vue
+++ components/arrangements/WorkerSelector.vue
@@ -11,8 +11,16 @@ const props = defineProps({
 /** define refs */
 const activeTab = ref(0);
 const tabs = ref([
-  { label: "従業員", key: "employees" },
-  { label: "外注先", key: "outsourcers" },
+  {
+    label: "従業員",
+    key: "employees",
+    windowItem: { value: 0, modelValue: props.employees },
+  },
+  {
+    label: "外注先",
+    key: "outsourcers",
+    windowItem: { value: 1, modelValue: props.outsourcers },
+  },
 ]);
 
 /** define constants */
@@ -34,26 +42,18 @@ const DRAGGABLE_CONFIG = {
       </v-tab>
     </v-tabs>
 
-    <!-- タブコンテンツ -->
     <v-window v-model="activeTab" class="fill-height">
-      <!-- 従業員タブ -->
-      <v-window-item :value="0" class="fill-height">
+      <v-window-item
+        v-for="tab in tabs"
+        :key="tab.key"
+        :value="tab.windowItem.value"
+        class="fill-height"
+      >
         <div class="pa-2 fill-height">
-          <draggable :model-value="employees" v-bind="DRAGGABLE_CONFIG">
-            <template #item="{ element }">
-              <MoleculesTagBase
-                v-bind="element"
-                :label="props.getWorkerName(element)"
-              />
-            </template>
-          </draggable>
-        </div>
-      </v-window-item>
-
-      <!-- 外注先タブ -->
-      <v-window-item :value="1" class="fill-height">
-        <div class="pa-2 fill-height">
-          <draggable :model-value="outsourcers" v-bind="DRAGGABLE_CONFIG">
+          <draggable
+            :model-value="tab.windowItem.modelValue"
+            v-bind="DRAGGABLE_CONFIG"
+          >
             <template #item="{ element }">
               <MoleculesTagBase
                 v-bind="element"
