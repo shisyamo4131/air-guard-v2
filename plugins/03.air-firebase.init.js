75b2906e9c3aa80344230a7540283d67c9e520c1
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-11-17 16:54:54 +0900

    change: air-firebase-v2-client-adapter を npm パッケージ化

--- plugins/03.air-firebase.init.js
+++ plugins/03.air-firebase.init.js
@@ -1,5 +1,6 @@
 import FireModel from "air-firebase-v2";
-import ClientAdapter from "air-firebase-v2-client-adapter";
+// import ClientAdapter from "air-firebase-v2-client-adapter";
+import ClientAdapter from "@shisyamo4131/air-firebase-v2-client-adapter";
 
 export default defineNuxtPlugin((app) => {
   FireModel.setAdapter(new ClientAdapter());
