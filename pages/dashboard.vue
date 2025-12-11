663b0e40bb38fb4569a0483a9dbd6243c91afa3c
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-12-11 11:17:01 +0900

    feat: replace MoleculesPopupCardWithTitle with air-card in dashboard.vue

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -17,76 +17,74 @@ const statistics = useStatisticsStore();
     <v-row v-if="auth.hasPermission('operation-billings:write')" dense>
       <v-col cols="12">
         <!-- 妥当性エラー稼働請求 -->
-        <MoleculesPopupCardWithTitle color="warning">
-          <template #title>
-            <v-icon size="small">mdi-alert</v-icon>
-            要修正稼働請求
+        <air-card color="warning" prepend-icon="mdi-alert" popup>
+          <template #title> 要修正稼働請求 </template>
+          <template #subtitle> 修正・確認が必要な稼働請求です。 </template>
+          <template #text>
+            <v-data-table
+              :headers="[
+                {
+                  title: '日付',
+                  key: 'dateAt',
+                  value: (item) =>
+                    dayjs(item.dateAt).format('YYYY年MM月DD日(ddd)'),
+                },
+                {
+                  title: '現場名',
+                  key: 'siteId',
+                  value: (item) => {
+                    const site = statistics.cachedSites?.[item.siteId] || null;
+                    return site ? site.name : 'loading...';
+                  },
+                },
+                {
+                  title: '内容',
+                  key: 'isInvalid',
+                  value: (item) => {
+                    switch (item.isInvalid) {
+                      case 'EMPTY_AGREEMENT':
+                        return '取極めが未選択です。';
+                      case 'EMPTY_BILLING_DATE':
+                        return '請求締日が未設定です。';
+                      default:
+                        return '不明なエラーです。';
+                    }
+                  },
+                },
+              ]"
+              :items="statistics.invalidOperationBillings"
+              :items-per-page="-1"
+              hide-default-footer
+              @click:row="
+                (_, { item }) => {
+                  router.push(`/billings/operations/${item.docId}`);
+                }
+              "
+            />
           </template>
-          <v-data-table
-            :headers="[
-              {
-                title: '日付',
-                key: 'dateAt',
-                value: (item) =>
-                  dayjs(item.dateAt).format('YYYY年MM月DD日(ddd)'),
-              },
-              {
-                title: '現場名',
-                key: 'siteId',
-                value: (item) => {
-                  const site = statistics.cachedSites?.[item.siteId] || null;
-                  return site ? site.name : 'loading...';
-                },
-              },
-              {
-                title: '内容',
-                key: 'isInvalid',
-                value: (item) => {
-                  switch (item.isInvalid) {
-                    case 'EMPTY_AGREEMENT':
-                      return '取極めが未選択です。';
-                    case 'EMPTY_BILLING_DATE':
-                      return '請求締日が未設定です。';
-                    default:
-                      return '不明なエラーです。';
-                  }
-                },
-              },
-            ]"
-            :items="statistics.invalidOperationBillings"
-            :items-per-page="-1"
-            hide-default-footer
-            @click:row="
-              (_, { item }) => {
-                router.push(`/billings/operations/${item.docId}`);
-              }
-            "
-          />
-        </MoleculesPopupCardWithTitle>
+        </air-card>
       </v-col>
     </v-row>
 
     <!-- 概要カード -->
     <v-row class="mb-6" dense>
       <v-col cols="12" sm="4">
-        <MoleculesPopupCardWithTitle>
-          <template #title>
-            <v-icon size="small">mdi-account-multiple</v-icon>
-            本日の稼働数
+        <air-card color="primary" prepend-icon="mdi-account-multiple" popup>
+          <template #title> 本日の稼働数 </template>
+          <template #text>
+            <span class="text-h5">{{ statistics.operationCount }}</span>
+            <span class="ml-2">稼働</span>
           </template>
-          <span class="text-h5">{{ statistics.operationCount }}</span>
-          <span class="ml-2">稼働</span>
-        </MoleculesPopupCardWithTitle>
+        </air-card>
       </v-col>
       <v-col cols="12" sm="4">
-        <MoleculesPopupCardWithTitle color="secondary">
-          <template #title>
-            <v-icon size="small">mdi-pickaxe</v-icon>
-            稼働中の現場
+        <air-card color="secondary" prepend-icon="mdi-pickaxe" popup>
+          <template #title> 稼働中の現場 </template>
+          <template #text>
+            <span class="text-h5">{{ statistics.siteCount }}</span>
+            <span class="ml-2">拠点</span>
           </template>
-          <span class="text-h5">{{ statistics.siteCount }}</span>
-          <span class="ml-2">拠点</span>
-        </MoleculesPopupCardWithTitle>
+        </air-card>
       </v-col>
     </v-row>
   </v-container>
