c82e7ab11a544a83ac4b31f948953e21750ebeb2
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-12-23 15:04:55 +0900

    component-test.vueから不要なコードを削除し、テンプレートを簡素化

--- pages/test/component-test.vue
+++ pages/test/component-test.vue
@@ -1,15 +1,6 @@
-<script setup>
-const search = ref(null);
-</script>
+<
+<script setup></script>
 
 <template>
-  <v-container class="d-flex align-center ga-2">
-    <air-text-field
-      hide-details
-      density="compact"
-      variant="solo-inverted"
-      single-line
-    />
-    <v-btn icon="mdi-plus" density="comfortable" />
-  </v-container>
+  <div></div>
 </template>
