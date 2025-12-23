2e07d902dbe33cc44b2730683999c6c46990cc37
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-12-23 15:04:06 +0900

    SpeedDialコンポーネントのpropsを削除し、ボタンの構成を簡素化

--- components/arrangements/SpeedDial.vue
+++ components/arrangements/SpeedDial.vue
@@ -1,63 +1,26 @@
 <script setup>
-/** define props */
-const props = defineProps({
-  direction: { type: String, default: "left" },
-  disabledEmployee: { type: Boolean, default: false },
-  disabledOutsourcer: { type: Boolean, default: false },
-  disabledCopy: { type: Boolean, default: false },
-  disabledPaste: { type: Boolean, default: false },
-  transition: { type: String, default: "slide-x-reverse-transition" },
-});
+const emit = defineEmits(["click:site-order", "click:add-schedule"]);
 
-/** define emits */
-const emit = defineEmits([
-  "click:add-employee",
-  "click:add-outsourcer",
-  "click:copy",
-  "click:paste",
-]);
-
-/** define constants */
-const fab = ref(false);
+const open = ref(false);
 const btns = ref([
   {
-    icon: "mdi-account",
-    onClick: () => emit("click:add-employee"),
-    disabled: props.disabledEmployee,
+    icon: "mdi-sort",
+    color: "info",
+    onClick: () => emit("click:site-order"),
   },
   {
-    icon: "mdi-handshake",
-    onClick: () => emit("click:add-outsourcer"),
-    disabled: props.disabledOutsourcer,
-  },
-  {
-    icon: "mdi-content-copy",
-    onClick: () => emit("click:copy"),
-    disabled: props.disabledCopy,
-  },
-  {
-    icon: "mdi-content-paste",
-    onClick: () => emit("click:paste"),
-    disabled: props.disabledPaste,
+    icon: "mdi-plus",
+    color: "secondary",
+    onClick: () => emit("click:add-schedule"),
   },
 ]);
 </script>
 
 <template>
-  <v-speed-dial v-model="fab" v-bind="props" location="left center">
-    <template #activator="{ props: activatorProps }">
-      <v-btn
-        v-bind="activatorProps"
-        size="x-small"
-        :icon="fab ? 'mdi-close' : 'mdi-plus'"
-        variant="elevated"
-      />
-    </template>
-    <v-btn
-      v-for="(btn, index) in btns"
-      :key="index"
-      v-bind="btn"
-      size="x-small"
-    />
-  </v-speed-dial>
+  <v-fab app icon variant="elevated">
+    <v-icon>{{ open ? "mdi-close" : "mdi-plus" }}</v-icon>
+    <v-speed-dial v-model="open" activator="parent">
+      <v-btn v-for="(btn, index) in btns" :key="index" v-bind="btn" />
+    </v-speed-dial>
+  </v-fab>
 </template>
