a0bf4386206a2ea329296c1f2d0830f034ca490d
shisyamo4131 <sevenstar_1226@hotmail.com>
2026-02-06 13:53:04 +0900

    commit

---
+++ components/SiteOperationSchedule/Draggable/Schedules/useIndex.js
@@ -0,0 +1,92 @@
+import * as Vue from "vue";
+import dayjs from "dayjs";
+import { SiteOperationSchedule } from "@/schemas";
+import { useBaseManager } from "@/composables/useBaseManager";
+
+export function useIndex(props, emit) {
+  /*****************************************************************************
+   * SETUP COMPOSABLES
+   *****************************************************************************/
+  const { logger, isDev } = useBaseManager(
+    "SiteOperationScheduleDraggableSchedules",
+  );
+
+  /*****************************************************************************
+   * SETUP STATES
+   *****************************************************************************/
+  // 内部管理用の schedule オブジェクト配列
+  const internalSchedules = Vue.ref(props.modelValue || []);
+  Vue.watch(
+    () => props.modelValue,
+    (newSchedules) => {
+      internalSchedules.value = newSchedules.map(
+        (s) => new SiteOperationSchedule(s),
+      );
+    },
+    { immediate: true, deep: true },
+  );
+
+  /*****************************************************************************
+   * METHODS
+   *****************************************************************************/
+  function handleUpdateModelValue(newSchedules) {
+    // ログ出力（開発環境のみ）
+    if (isDev) {
+      logger.info({
+        message: "Updating modelValue with new schedules",
+        newSchedules,
+      });
+    }
+
+    // siteId, shiftType, date, displayOrder を更新
+    newSchedules.forEach((schedule, index) => {
+      schedule.siteId = props.siteId;
+      schedule.shiftType = props.shiftType;
+      schedule.dateAt = dayjs // 実行環境に依存せず Asia/Tokyo として扱う
+        .tz(props.date, "Asia/Tokyo")
+        .startOf("day")
+        .toDate();
+      schedule.displayOrder = index;
+    });
+
+    // 内部状態を更新（Optimistic Updates）
+    internalSchedules.value = newSchedules;
+
+    // 親コンポーネントに更新を通知
+    emit("update:modelValue", newSchedules);
+  }
+  /*****************************************************************************
+   * COMPUTED PROPERTIES
+   *****************************************************************************/
+  // draggable コンポーネントに渡す属性
+  const attrs = Vue.computed(() => {
+    return {
+      disabled: props.disabled,
+      group: { name: props.groupName },
+      handle: props.handle,
+      itemKey: props.itemKey,
+      modelValue: internalSchedules.value,
+      "onUpdate:modelValue": handleUpdateModelValue,
+
+      // 以下、スマホやタブレット端末においてドラッグ中の要素をPCと同様に取り扱うための追加設定
+      // この設定を行わないと、ドラッグ中の要素が親コンテナの描画範囲からはみ出ないなどの問題が発生する
+      ghostClass: "sortable-ghost",
+      forceFallback: true,
+      fallbackOnBody: true,
+      appendTo: "body",
+    };
+  });
+
+  // デフォルトスロットに渡すプロパティ
+  const defaultSlotProps = Vue.computed(() => {
+    return {
+      isDraggable: !props.disabled,
+      removable: !props.disabled,
+    };
+  });
+
+  return {
+    attrs,
+    defaultSlotProps,
+  };
+}
