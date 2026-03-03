9d28f5d8d4123e89c69bf1f1cbc7c2bb67a06718
shisyamo4131 <sevenstar_1226@hotmail.com>
2026-03-03 15:09:43 +0900

    commit

--- components/Agreement/Card/index.vue
+++ components/Agreement/Card/index.vue
@@ -3,6 +3,16 @@
  * @file ./components/Agreement/Card/index.vue
  * @description 取極め情報表示コンポーネント
  * @author shisyamo4131
+ *
+ * @prop {Object} agreement - 取極め情報オブジェクト
+ * @prop {Boolean} disabled - コンポーネント全体の操作不可フラグ
+ * @prop {Boolean} loading - ローディング状態フラグ
+ * @prop {Boolean} isSelected - 選択状態フラグ
+ * @prop {Boolean} showEdit - 編集アイコン表示フラグ
+ * @prop {Boolean} showSelect - 選択アイコン表示フラグ
+ *
+ * @emit click:select - 選択アイコンクリックイベント
+ * @emit click:edit - 編集アイコンクリックイベント
  *****************************************************************************/
 import { useDefaults } from "vuetify";
 import { useIndex } from "./useIndex";
@@ -10,61 +20,89 @@ import { useIndex } from "./useIndex";
 /** SETUP PROPS */
 const _props = defineProps({
   agreement: { type: Object, default: null },
+  disabled: { type: Boolean, default: false },
   isSelected: { type: Boolean, default: false },
+  loading: { type: Boolean, default: false },
+  showEdit: { type: Boolean, default: false },
+  showSelect: { type: Boolean, default: false },
 });
 const props = useDefaults(_props, "AgreementCard");
 
-const { label, formatted, dayType, shiftType } = useIndex(props);
+const emit = defineEmits(["click:select", "click:edit"]);
+
+/** SETUP COMPOSABLE */
+const {
+  dateAtLabel,
+  formatted,
+  dayType,
+  shiftTypeColor,
+  shiftTypeIcon,
+  shiftTypeLabel,
+  selectIconProps,
+  actionProps,
+} = useIndex(props, emit);
 </script>
 
 <template>
-  <v-card prepend-icon="mdi-file-document-outline">
-    <!-- タイトル -->
-    <template #title>
-      <span class="text-subtitle-1">{{ label }}</span>
-    </template>
-
-    <!-- 選択中アイコン -->
-    <template v-if="props.isSelected" #append>
-      <v-chip
-        prepend-icon="mdi-check-circle-outline"
-        text="選択中"
-        color="info"
-        label
-        density="compact"
-      />
-    </template>
+  <v-card style="min-width: 234px">
+    <!-- ヘッダー（勤務区分表示部）-->
+    <v-card-text :class="['pa-2', `bg-${shiftTypeColor}`]" style="height: 36px">
+      <div class="d-flex align-center">
+        <v-icon :icon="shiftTypeIcon" class="me-1" />
+        <span>{{ shiftTypeLabel }}</span>
+        <v-spacer />
+        <v-icon v-if="props.showSelect" v-bind="selectIconProps" />
+      </div>
+    </v-card-text>
 
     <!-- 単価情報テーブル -->
-    <v-table density="compact">
+    <v-table class="rounded-0 text-caption" density="compact">
       <tbody>
         <tr>
-          <td>区分</td>
+          <td>適用開始</td>
+          <td>{{ dateAtLabel }}</td>
+        </tr>
+        <tr>
+          <td>曜日区分</td>
           <td>
-            <DayTypeChip :day-type="dayType" label density="compact" />
-          </td>
-          <td>
-            <ShiftTypeChip :shift-type="shiftType" label density="compact" />
+            <DayTypeChip
+              :day-type="dayType"
+              label
+              density="compact"
+              size="small"
+            />
           </td>
         </tr>
         <tr>
-          <td>基本</td>
+          <td>基本単価</td>
           <td>{{ formatted.unitPriceBase }}</td>
+        </tr>
+        <tr>
+          <td>残業単価</td>
           <td>{{ formatted.overTimeUnitPriceBase }}</td>
         </tr>
         <tr>
-          <td>資格</td>
+          <td>資格単価</td>
           <td>{{ formatted.unitPriceQualified }}</td>
+        </tr>
+        <tr>
+          <td>資格残業</td>
           <td>{{ formatted.overTimeUnitPriceQualified }}</td>
         </tr>
       </tbody>
     </v-table>
+
+    <v-divider />
+
+    <!-- 編集アクション -->
+    <v-card-actions v-if="props.showEdit" class="py-0" style="min-height: 36px">
+      <MoleculesActionsEdit v-bind="actionProps" />
+    </v-card-actions>
   </v-card>
 </template>
 
 <style scoped>
-td:nth-child(2),
-td:nth-child(3) {
-  text-align: center;
+td:nth-child(2) {
+  text-align: right;
 }
 </style>
--- components/Agreement/Card/useIndex.js
+++ components/Agreement/Card/useIndex.js
@@ -1,31 +1,143 @@
+/*****************************************************************************
+ * @file ./components/Agreement/Card/useIndex.js
+ * @description AgreementCard 専用コンポーザブル
+ * @author shisyamo4131
+ *****************************************************************************/
 import * as Vue from "vue";
 import dayjs from "dayjs";
 import { formatCurrency } from "@/utils/formats/util";
+import { useConstants } from "@/composables/useConstants";
 
-export function useIndex(props) {
-  const label = Vue.computed(() => {
-    return `${dayjs(props.agreement.dateAt).format("YYYY年MM月DD日(ddd)")} ～`;
+/**
+ * AgreementCard 専用コンポーザブル
+ * @param {Object} props - AgreementCard コンポーネントの props を受け取るオブジェクト
+ * @param {Object} emit - AgreementCard コンポーネントの emit 関数
+ * @returns {Object} AgreementCard コンポーネントで使用する computed プロパティを返すオブジェクト
+ * @returns {ComputedRef<String>} dateAtLabel - 契約の開始日をフォーマットして表示するための computed プロパティ
+ * @returns {ComputedRef<String>} dayTypeLabel - 契約の勤務形態に応じて、勤務形態のラベルを表示するための computed プロパティ
+ * @returns {ComputedRef<String>} shiftTypeColor - 契約のシフトタイプに応じて、シフトタイプの色を表示するための computed プロパティ
+ * @returns {ComputedRef<String>} shiftTypeLabel - 契約のシフトタイプに応じて、シフトタイプのラベルを表示するための computed プロパティ
+ * @returns {ComputedRef<Object>} formatted - 契約の料金情報をフォーマットして表示するための computed プロパティ
+ * @returns {ComputedRef<String>} selectIcon - 選択チェックボックスのアイコンを表示するための computed プロパティ
+ */
+export function useIndex(props, emit) {
+  const { DAY_TYPE, SHIFT_TYPE } = useConstants();
+
+  /**
+   * 契約の開始日をフォーマットして表示するためのcomputedプロパティ。
+   * 契約の開始日を"YYYY年MM月DD日(ddd)"の形式で表示するために、dayjsライブラリを使用。
+   */
+  const dateAtLabel = Vue.computed(() => {
+    return `${dayjs(props.agreement.dateAt).format("YYYY年MM月DD日(ddd)")}`;
   });
 
+  /**
+   * 契約の料金情報をフォーマットして表示するためのcomputedプロパティ。
+   * 契約の料金情報を通貨形式で表示するために、formatCurrency関数を使用。
+   * また、契約のbillingUnitTypeに応じて、単位を"時間"または"人工"として表示する。
+   */
   const formatted = Vue.computed(() => {
-    const unit = props.agreement.billingUnitType === "HOUR" ? "時間" : "人工";
+    const agreement = props.agreement;
+    const unit = agreement.billingUnitType === "HOUR" ? "/時間" : "/人工";
+    const overtimeUnit = "/時間";
+    const unitPriceBase = formatCurrency(agreement.unitPriceBase);
+    const overTimeUnitPriceBase = formatCurrency(
+      agreement.overtimeUnitPriceBase,
+    );
+    const unitPriceQualified = formatCurrency(agreement.unitPriceQualified);
+    const overTimeUnitPriceQualified = formatCurrency(
+      agreement.overtimeUnitPriceQualified,
+    );
     return {
-      unitPriceBase:
-        formatCurrency(props.agreement.unitPriceBase) + " / " + unit,
-      overTimeUnitPriceBase:
-        formatCurrency(props.agreement.overtimeUnitPriceBase) + " / " + "時間",
-      unitPriceQualified:
-        formatCurrency(props.agreement.unitPriceQualified) + " / " + unit,
-      overTimeUnitPriceQualified:
-        formatCurrency(props.agreement.overtimeUnitPriceQualified) +
-        " / " +
-        "時間",
+      unitPriceBase: unitPriceBase + unit,
+      overTimeUnitPriceBase: overTimeUnitPriceBase + overtimeUnit,
+      unitPriceQualified: unitPriceQualified + unit,
+      overTimeUnitPriceQualified: overTimeUnitPriceQualified + overtimeUnit,
+    };
+  });
+
+  /**
+   * 契約の勤務形態に応じて、勤務形態のラベルを表示するためのcomputedプロパティ。
+   * DAY_TYPEとSHIFT_TYPEの定数を使用して、契約の勤務形態に対応するラベルを取得。
+   * もし、契約の勤務形態が定数に存在しない場合は、空文字列を返す。
+   */
+  const dayTypeLabel = Vue.computed(() => {
+    return DAY_TYPE.value[props.agreement.dayType]?.title || "";
+  });
+
+  /**
+   * 契約のシフトタイプに応じて、シフトタイプの色を表示するためのcomputedプロパティ。
+   * SHIFT_TYPEの定数を使用して、契約のシフトタイプに対応する色を取得。
+   * もし、契約のシフトタイプが定数に存在しない場合は、undefinedを返す。
+   */
+  const shiftTypeColor = Vue.computed(() => {
+    return SHIFT_TYPE.value[props.agreement.shiftType]?.color || undefined;
+  });
+
+  /**
+   * 契約のシフトタイプに応じて、シフトタイプのラベルを表示するためのcomputedプロパティ。
+   * SHIFT_TYPEの定数を使用して、契約のシフトタイプに対応するラベルを取得。
+   * もし、契約のシフトタイプが定数に存在しない場合は、空文字列を返す。
+   */
+  const shiftTypeIcon = Vue.computed(() => {
+    return SHIFT_TYPE.value[props.agreement.shiftType]?.icon || undefined;
+  });
+
+  /**
+   * 契約のシフトタイプに応じて、シフトタイプのラベルを表示するためのcomputedプロパティ。
+   * SHIFT_TYPEの定数を使用して、契約のシフトタイプに対応するラベルを取得。
+   * もし、契約のシフトタイプが定数に存在しない場合は、空文字列を返す。
+   */
+  const shiftTypeLabel = Vue.computed(() => {
+    return SHIFT_TYPE.value[props.agreement.shiftType]?.title || "";
+  });
+
+  /**
+   * 選択チェックボックスのアイコンを表示するためのcomputedプロパティ。
+   * props.isSelectedがtrueの場合は、"mdi-checkbox-outline"を表示し、falseの場合は"mdi-checkbox-blank-outline"を表示する。
+   */
+  const selectIcon = Vue.computed(() => {
+    return props.isSelected
+      ? "mdi-checkbox-outline"
+      : "mdi-checkbox-blank-outline";
+  });
+
+  /**
+   * 選択チェックボックスコンポーネントに引き渡すプロパティ
+   */
+  const selectIconProps = Vue.computed(() => {
+    return {
+      disabled: props.disabled,
+      loading: props.loading,
+      icon: selectIcon.value,
+      onClick: () => emit("click:select"),
+    };
+  });
+
+  /**
+   * 編集アクションコンポーネントに引き渡すプロパティ
+   */
+  const actionProps = Vue.computed(() => {
+    return {
+      density: "comfortable",
+      disabled: props.disabled,
+      loading: props.loading,
+      icon: true,
+      size: "small",
+      "onClick:edit": () => emit("click:edit"),
     };
   });
   return {
-    label,
+    dateAtLabel,
     dayType: Vue.computed(() => props.agreement.dayType),
+    dayTypeLabel,
     shiftType: Vue.computed(() => props.agreement.shiftType),
+    shiftTypeColor,
+    shiftTypeIcon,
+    shiftTypeLabel,
     formatted,
+    selectIcon,
+    selectIconProps,
+    actionProps,
   };
 }
