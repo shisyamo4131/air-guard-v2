663259d87453ad0a9de04bad37666c67f392b191
shisyamo4131 <sevenstar_1226@hotmail.com>
2026-05-09 16:12:38 +0900

    終了現場検索画面の新設

---
+++ pages/sites/terminated.vue
@@ -0,0 +1,45 @@
+<script setup>
+/*****************************************************************************
+ * @file pages/sites/terminated.vue
+ * @description 終了現場検索ページ
+ *****************************************************************************/
+import { useRouter } from "vue-router";
+import { Site } from "@/schemas";
+import { useDocuments } from "@/composables/dataLayers/useDocuments";
+
+defineOptions({ name: "sites-terminated-index" });
+
+/*****************************************************************************
+ * DEFINE STATES
+ *****************************************************************************/
+const search = ref("");
+
+/*****************************************************************************
+ * SETUP COMPOSABLES
+ *****************************************************************************/
+const router = useRouter();
+
+const defaultOption = ["where", "status", "==", Site.STATUS_TERMINATED];
+const options = computed(() => {
+  if (!search.value) {
+    return [defaultOption, ["orderBy", "updatedAt", "desc"], ["limit", 10]];
+  } else {
+    return [defaultOption, ["orderBy", "code", "desc"]];
+  }
+});
+const { docs } = useDocuments("Site", {
+  search,
+  options,
+});
+</script>
+
+<template>
+  <TemplatesFixedHeightContainer>
+    <SitesManager
+      :docs="docs"
+      v-model:search="search"
+      :items-per-page="20"
+      @click:detail="(item) => router.push(`/sites/${item.docId}`)"
+    />
+  </TemplatesFixedHeightContainer>
+</template>
