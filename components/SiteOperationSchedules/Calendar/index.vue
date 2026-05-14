6df6e651abeccc939470ab91ff26cfa33411af97
shisyamo4131 <sevenstar_1226@hotmail.com>
2026-05-14 13:10:18 +0900

    表示対象の日付指定を modelValue に変更し、内部で状態を管理できるように修正

--- components/SiteOperationSchedules/Calendar/index.vue
+++ components/SiteOperationSchedules/Calendar/index.vue
@@ -3,7 +3,7 @@
  * @file ./components/SiteOperationSchedules/Calendar/index.vue
  * @description A component to display `SiteOperationSchedules` as calendar format.
  *
- * @property {Object} dateAt - The date to display in the calendar (default: new Date()).
+ * @property {Object} modelValue - The date to display in the calendar (default: new Date()).
  * @property {Array} schedules - An array of schedule objects to display on the calendar.
  *****************************************************************************/
 import { useDefaults } from "vuetify";
@@ -12,11 +12,44 @@ import { useDefaults } from "vuetify";
  * DEFINE PROPS & EMITS
  *****************************************************************************/
 const _props = defineProps({
-  dateAt: { type: Object, default: () => new Date() },
   events: { type: Array, default: () => [] },
+  modelValue: { type: Object, default: () => new Date() },
 });
 const props = useDefaults(_props, "SiteOperationSchedulesCalendar");
-const emit = defineEmits(["update:date-range", "click:event"]);
+const emit = defineEmits([
+  "update:modelValue",
+  "update:date-range",
+  "click:event",
+]);
+
+/*****************************************************************************
+ * DEFINE STATES
+ *****************************************************************************/
+const internalModelValue = ref(props.modelValue);
+
+/*****************************************************************************
+ * WATCHERS
+ *****************************************************************************/
+watch(
+  () => props.modelValue,
+  (newVal) => {
+    internalModelValue.value = newVal;
+  },
+  { immediate: true },
+);
+
+/*****************************************************************************
+ * COMPUTED
+ *****************************************************************************/
+const computedModelValue = computed({
+  get() {
+    return internalModelValue.value;
+  },
+  set(v) {
+    internalModelValue.value = v;
+    emit("update:modelValue", v);
+  },
+});
 
 /*****************************************************************************
  * METHODS
@@ -27,15 +60,15 @@ function onClickEvent(nativeEvent, { event }) {
 </script>
 
 <template>
-  <div>
+  <div class="d-flex flex-column">
     <MoleculesMonthSelector
       class="pb-2"
-      :model-value="props.dateAt"
+      v-model="computedModelValue"
       @date-range="emit('update:date-range', $event)"
     />
     <air-calendar
       style="min-height: 520px"
-      :model-value="props.dateAt"
+      :model-value="computedModelValue"
       :events="props.events"
       @click:event="onClickEvent"
     />
