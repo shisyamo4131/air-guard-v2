03a4efc84f3dd8196690c09c55b18992e9b83258
shisyamo4131 <sevenstar_1226@hotmail.com>
2026-07-04 19:05:38 +0900

    commit

--- pages/super-user/index.vue
+++ pages/super-user/index.vue
@@ -1,6 +1,16 @@
 <script setup>
+/*****************************************************************************
+ * @file ./pages/super-user/index.vue
+ * @description スーパーユーザー専用ページ
+ *****************************************************************************/
 import { useAuthStore } from "@/stores/useAuthStore";
 import { httpsCallable } from "firebase/functions";
+import { useMessagesStore } from "@/stores/useMessagesStore";
+import { useErrorsStore } from "@/stores/useErrorsStore";
+import { useLogger } from "@/composables/useLogger";
+import { useLoadingsStore } from "@/stores/useLoadingsStore";
+
+const { $functions } = useNuxtApp();
 
 /*****************************************************************************
  * SETUP AUTH STORE
@@ -9,22 +19,39 @@ const auth = useAuthStore();
 const { companyId } = auth;
 
 /*****************************************************************************
+ * SETUP MESSAGES STORE
+ *****************************************************************************/
+const messages = useMessagesStore();
+
+/*****************************************************************************
+ * SETUP LOGGER COMPOSABLE
+ *****************************************************************************/
+const logger = useLogger("pages/super-user/index.vue", useErrorsStore());
+
+/*****************************************************************************
+ * SETUP LOADINGS STORE
+ *****************************************************************************/
+const loadings = useLoadingsStore();
+
+/*****************************************************************************
  * METHODS
  *****************************************************************************/
 async function onClickRebuildAllHistories() {
+  const key = loadings.add("Re-building all histories...");
   try {
     const callable = httpsCallable($functions, "rebuildAllHistories");
     const result = await callable({ companyId });
-    console.log(result);
+    messages.add(result.data.message);
   } catch (error) {
-    console.error(error);
+    logger.error({ error });
+  } finally {
+    loadings.remove(key);
   }
 }
 </script>
 
 <template>
   <v-container>
-    {{ auth.roles }}
     <v-card
       title="現場入場履歴再構築"
       subtitle="全会社の現場入場履歴を再構築します。"
@@ -32,7 +59,7 @@ async function onClickRebuildAllHistories() {
       <template #actions>
         <v-btn
           color="primary"
-          label="実行"
+          text="実行"
           @click="onClickRebuildAllHistories"
         />
       </template>
